c22eb6e54a92c5ae9416c2b4a47d4d1d215b2bbd
Arjunaadlina <107385207+Arjunaadlina@users.noreply.github.com>
2022-10-17 15:58:10 +0700

    Add files via upload

---
+++ spam.js
@@ -0,0 +1,19 @@
+//berhasil cuy gawe spam ig
+
+var pesan = prompt("kosongkan saja, ketik di dm \nsoalnya eror");
+var serangan = parseInt(prompt("mau berapa kali?",10));
+
+var event = new InputEvent(`input`,{
+    bubbles : true
+});
+window.InputEvent = window.Event || window.InputEvent;
+
+var text =  document.querySelector(" div._acrb div._ab8w._ab94._ab99._ab9f._ab9m._ab9o._abbh._abcm textarea");
+
+    for (var i=1;i<serangan;i++) {
+        text.innerHTML == pesan;
+        text.dispatchEvent(event);
+        document.querySelector("div._ab8w._ab94._ab99._ab9f._ab9m._ab9p._abbi._abcm button").click();
+    }
+//zhiend
+
